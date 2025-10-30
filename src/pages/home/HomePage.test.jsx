456e2cb245fc81f4fbba0c5ba83d243d32a0affb
Sasha <aleksvladpinchuk@gmail.com>
2025-10-30 21:26:23 +0300

    test add to homepage

--- src/pages/home/HomePage.test.jsx
+++ src/pages/home/HomePage.test.jsx
@@ -3,13 +3,17 @@ import { render, screen, within } from "@testing-library/react";
 import { MemoryRouter } from "react-router";
 import { HomePage } from "./HomePage";
 import axios from "axios";
+import userEvent from "@testing-library/user-event";
 
 vi.mock("axios");
 
 describe("HomePage component", () => {
   let loadCart;
+  let user;
 
   beforeEach(() => {
+    user = userEvent.setup();
+
     loadCart = vi.fn();
 
     axios.get.mockImplementation(async (urlPath) => {
@@ -64,5 +68,49 @@ describe("HomePage component", () => {
     expect(
       within(productContainers[1]).getByText("Intermediate Size Basketball")
     ).toBeInTheDocument();
+  });
+
+  it("add to cart button works", async () => {
+    render(
+      <MemoryRouter>
+        <HomePage cart={[]} loadCart={loadCart} />
+      </MemoryRouter>
+    );
+
+    const productContainers = await screen.findAllByTestId("product-container");
+
+    const quantitySelector1 = within(productContainers[0]).getByTestId(
+      "quantity-selector"
+    );
+
+    await user.selectOptions(quantitySelector1, "2");
+
+    const addToCartButton1 = within(productContainers[0]).getByTestId(
+      "add-to-cart-button"
+    );
+
+    await user.click(addToCartButton1);
+
+    const quantitySelector2 = within(productContainers[1]).getByTestId(
+      "quantity-selector"
+    );
+
+    await user.selectOptions(quantitySelector2, "3");
+
+    const addToCartButton2 = within(productContainers[1]).getByTestId(
+      "add-to-cart-button"
+    );
+
+    await user.click(addToCartButton2);
+
+    expect(axios.post).toHaveBeenNthCalledWith(1, "/api/cart-items", {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    });
+    expect(axios.post).toHaveBeenNthCalledWith(2, "/api/cart-items", {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 3,
+    });
+    expect(loadCart).toHaveBeenCalledTimes(2);
   });
 });
